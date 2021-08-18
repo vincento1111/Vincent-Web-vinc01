881510584c77423fafa4f1042216fb8d758796d6
vincent <vincentscuurhof2009@gmail.com>
2021-08-18 10:37:40 +0200

    test

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  template: `
+    <section class="hero is-primary is-bold">
+    <div class="hero-body">
+    <div class="container">
+      <h1 class="Title">Contact Us!</h1>
+    </div>
+    </div>
+    </section>
+
+    <section class="section">
+    <div class="container">
+      
+      <form>
+      
+      <div class="field">
+        <label class="label">Email</label>
+        <input type="text" name="name" class="input">
+      </div>
+      
+      <div class="field">
+        <label class="label">Email</label>
+        <input type="text" name="name" class="input">
+      </div>
+      
+      <div class="field">
+      <label class="label">Email</label>
+      <input type="text" name="name" class="input">
+      </div>
+
+      </form>
+
+
+  `,
+  styles: [
+  ]
+})
+export class ContactComponent implements OnInit {
+
+  //name;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
