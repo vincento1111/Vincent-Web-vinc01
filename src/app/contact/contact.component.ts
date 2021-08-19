79b45aca38987db5eb319b4c868e1bdee1953613
vincent <vincentscuurhof2009@gmail.com>
2021-08-19 11:07:34 +0200

    skifter plads

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,9 @@ import { Component, OnInit } from '@angular/core';
     <section class="section">
     <div class="container">
       
+
+    1. template
+    
       <form>
       
       <div class="field">
